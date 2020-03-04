2c8d3aaae90b46e4b6eecf40fe32ab52664cc15a
sghctoma <sghctoma@gmail.com>
2020-03-04 11:47:27 +0100

    Add history feature + refactor code

--- docs/lacerda.js
+++ docs/lacerda.js
@@ -1,81 +1,136 @@
-var randInt = function (max) {
-    return Math.floor(Math.random() * max);
+class Card {
+    constructor(action, mission, travel) {
+        this.action = action;
+        this.mission = mission;
+        this.travel = travel;
+    }
 }
 
-/*
- * Randomly shuffle an array
- * https://stackoverflow.com/a/2450976/1293256
- * @param  {Array} array The array to shuffle
- * @return {String}      The first item in the shuffled array
- */
-var shuffle = function (array) {
+class State {
+    constructor(action, mission, hexTiebreaker, contractTiebreaker, techTiebreaker) {
+        this.action = action;
+        this.mission = mission;
+        this.hexTiebreaker = hexTiebreaker;
+        this.contractTiebreaker = contractTiebreaker;
+        this.techTiebreaker = techTiebreaker;
+        this.primaryTurnorder = null;
+        this.secondaryTurnorder = null;
+    }
+}
 
-	var currentIndex = array.length;
-	var temporaryValue, randomIndex;
+class Lacerda {
 
-	// While there remain elements to shuffle...
-	while (0 !== currentIndex) {
-		// Pick a remaining element...
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex -= 1;
+    constructor() {
+        // [action, mission, travel]
+        this.cards = [
+            new Card(1, 1, true),
+            new Card(1, 2, true),
+            new Card(1, 2, true),
+            new Card(1, 3, true),
+            new Card(1, 3, false),
+            new Card(2, 1, true),
+            new Card(2, 1, false),
+            new Card(2, 2, true),
+            new Card(2, 3, true),
+            new Card(3, 1, true),
+            new Card(3, 2, false),
+            new Card(3, 3, true)
+        ];
 
-		// And swap it with the current element.
-		temporaryValue = array[currentIndex];
-		array[currentIndex] = array[randomIndex];
-		array[randomIndex] = temporaryValue;
-	}
+        this.shuffledIndexes = [...Array(12).keys()];
+        this.shuffle();
 
-	return array;
-
-};
-
-var draw = function () {
-    current_round += 1;
-
-    if (shuffled.length == 0) {
-        shuffled = [...Array(12).keys()];
-        shuffle(shuffled);
-        contribute = true;
+        this.currentRound = 0;
+        this.currentPhase = null;
+        this.history = [];
     }
 
-    var n = shuffled.splice(0, 1)[0];
-    var card = cards[n];
-
-    var ret = [
-        card[0],                    // action to take
-        contribute ? card[1] : 0,   // mission to contribute to
-        randInt(12)                 // hex tiebreaker
-    ]
-
-    if (card[2]) {
-        primary_turnorder = randInt(4);
-        tmp = [0, 1, 2, 3];
-        tmp.splice(primary_turnorder, 1)
-        secondary_turnorder = tmp[randInt(3)];
-
-        ret = ret.concat([primary_turnorder, secondary_turnorder]);
+    randInt(max) {
+        return Math.floor(Math.random() * max);
     }
 
-    return ret;
-};
+    /*
+    * Randomly shuffle an array
+    * Original: https://stackoverflow.com/a/2450976/1293256
+    */
+    shuffle() {
+        var currentIndex = this.shuffledIndexes.length;
+        var temporaryValue, randomIndex;
 
-// [action, mission, travel]
-var cards = [
-    [1, 1, true],
-    [1, 2, true],
-    [1, 2, true],
-    [1, 3, true],
-    [1, 3, false],
-    [2, 1, true],
-    [2, 1, false],
-    [2, 2, true],
-    [2, 3, true],
-    [3, 1, true],
-    [3, 2, false],
-    [3, 3, true]
-];
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
 
-var contribute = false;
-var shuffled = [...Array(12).keys()];
+            // And swap it with the current element.
+            temporaryValue = this.shuffledIndexes[currentIndex];
+            this.shuffledIndexes[currentIndex] = this.shuffledIndexes[randomIndex];
+            this.shuffledIndexes[randomIndex] = temporaryValue;
+        }
+    };
 
-shuffle(shuffled);
+    draw() {
+        if (this.shuffledIndexes.length == 0) {
+            this.shuffledIndexes = [...Array(12).keys()];
+            this.shuffle();
+        }
+
+        var n = this.shuffledIndexes.splice(0, 1)[0];
+        var card = this.cards[n];
+        var state = new State(
+            card.action,
+            this.currentRound > this.cards.length ? card.mission : null,
+            this.randInt(12),
+            this.randInt(6),
+            this.randInt(4));
+
+        if (card.travel) {
+            var primaryTurnorder = this.randInt(4);
+            var tmp = [0, 1, 2, 3];
+            tmp.splice(primaryTurnorder, 1)
+            var secondaryTurnorder = tmp[this.randInt(3)];
+
+            state.primaryTurnorder = primaryTurnorder;
+            state.secondaryTurnorder = secondaryTurnorder;
+        }
+
+        this.history.push(state);
+    }
+
+    nextState() {
+        this.currentRound += 1;
+        if (this.history.length < this.currentRound) {
+            this.draw();
+        }
+
+        return this.history[this.currentRound-1];
+    }
+
+    prevState() {
+        if (this.currentRound == 1) {
+            return null;
+        } else {
+            this.currentRound -= 1;
+            return this.history[this.currentRound-1];
+        }
+    }
+}
+
+lacerda = new Lacerda();
+console.debug(lacerda.nextState());
+console.debug(lacerda.nextState());
+console.debug(lacerda.nextState());
+
+console.debug(lacerda.prevState());
+console.debug(lacerda.prevState());
+console.debug(lacerda.prevState());
+console.debug(lacerda.prevState());
+
+//console.debug(lacerda.nextState());
+
+console.debug("------------------------------");
+
+console.debug(lacerda.history);
+console.debug(lacerda.currentRound);
+
