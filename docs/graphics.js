3004d3354b146fd643d120e9a95526d509bc9343
sghctoma <sghctoma@gmail.com>
2020-03-17 22:41:19 +0100

    Make it all work with new assets
    Yeah, I know, this couldn't be more vague. Lots of modifications here,
    and I forgot to commit often. Blaming the pandemic :)
    Anyways, it's mainly just layout stuff, image offsets, positions, etc.

--- docs/graphics.js
+++ docs/graphics.js
@@ -1,23 +1,40 @@
 class Display {
     constructor(lacerda) {
         this.lacerda = lacerda;
+        this.width = 845;
+        this.height = 1515;
 
         this.stage = new Konva.Stage({
             container: 'container',
-            width: 380,
-            height: 726
+            width: this.width,
+            height: this.height,
         });
         this.layer = new Konva.Layer();
 
         this.sources = {
-            background: '/assets/background.png',
+            // start screen
             startscreen: '/assets/startscreen.png',
-            button_reveal: '/assets/button_reveal.png',
-            button_next: '/assets/button_next.png',
-            shuttle: '/assets/shuttle.png',
-            shuttle_nogo: '/assets/shuttle_nogo.png',
-            shuttle_go_left: '/assets/shuttle_go_left.png',
-            shuttle_go_right: '/assets/shuttle_go_right.png'
+            init_shuttle: '/assets/shuttlephasebutton-hd@3x.png',
+            first_round: '/assets/first-round-button-hd@3x.png',
+
+            // general
+            background: '/assets/background.png',
+            backbutton: '/assets/backbutton@3x.png',
+
+            // colonization phase
+            colonization_phase_indicator: '/assets/2@3x.png',
+
+            // colonization phase; travel
+            shuttle: '/assets/shuttle-icon-centered@3x.png',
+            travel_arrows: '/assets/travel-arrows-left-right@3x.png',
+            shuttle_phase_indicator: '/assets/shuttle-phase-graphical-indicator@3x.png',
+            
+            // colonization phase; no travel
+            shuttle_nogo: '/assets/no-travel-icon@3x.png',
+            next_round: '/assets/next-round-button-hd@3x.png',
+
+            // shuttle phase
+            shuttle_phase_button: '/assets/initiateshuttlephasebutton@3x.png',
         };
         this.labels = {};
         this.shapes = {};
@@ -50,13 +67,28 @@ class Display {
         this.layer.draw();
     }
 
-    renderScreen() {
+    setupLayer() {
         this.layer.add(this.images.background);
+        this.layer.add(this.buttons.backbutton);
+        this.layer.add(this.labels.status);
+        this.layer.add(this.images.colonization_phase_indicator);
+        this.layer.add(this.images.shuttle_phase_indicator);
+        this.layer.add(this.shapes.active_mission);
+        this.layer.add(this.labels.mission);
+        this.layer.add(this.labels.action);
+            
+        this.layer.add(this.buttons.init_shuttle);
+        this.layer.add(this.images.shuttle);
+        this.layer.add(this.images.travel_arrows);
 
+        this.layer.add(this.buttons.next_round);
+        this.layer.add(this.images.shuttle_nogo);
+    }
+
+    renderScreen() {
         var state = this.lacerda.currentState();
 
         // status bar
-        this.layer.add(this.labels.status);
         this.labels.status.text(
             "ROUND " +
             this.lacerda.currentRound + 
@@ -65,27 +97,45 @@ class Display {
             " PHASE");
 
         // phase marker
-        //TODO
+        if (this.lacerda.currentPhase == 'COLONISATION' && state.turnOrders != null) {
+            this.images.colonization_phase_indicator.show();
+            this.images.shuttle_phase_indicator.hide();
+        } else {
+            this.images.colonization_phase_indicator.hide();
+            this.images.shuttle_phase_indicator.show();
+        }
 
         // mission marker
-        if (state.mission == null) {
-            //this.labels.mission.text('X');
-            this.layer.add(this.shapes.inactive_mission);
+        if (state.mission != null) {
+            this.labels.mission.text(['A', 'B', 'C'][state.mission-1]);
+            this.shapes.active_mission.show();
         } else {
-            //this.labels.mission.text(['A', 'B', 'C'][state.mission]);
-            this.layer.add(this.shapes.active_mission);
+            this.labels.mission.text('');
+            this.shapes.active_mission.hide();
         }
-        this.layer.add(this.labels.mission);
+
+        // current action
+        this.labels.action.text(state.action);
         
         // buttons
-        if (state.turnOrders != null) {
-            this.layer.add(this.buttons.init_shuttle);
-            this.layer.add(this.images.shuttle);
-            this.layer.add(this.images.shuttle_go_left);
-            this.layer.add(this.images.shuttle_go_right);
+        if (this.lacerda.currentPhase == 'SHUTTLE') {
+            this.buttons.init_shuttle.hide();
+            this.images.shuttle.hide();
+            this.images.travel_arrows.show();
+            //XXX show turn order
+            this.buttons.next_round.show();
+        } else if (state.turnOrders != null) {
+            this.buttons.init_shuttle.show();
+            this.images.shuttle.show();
+            this.images.travel_arrows.show();
+            this.buttons.next_round.hide();
+            this.images.shuttle_nogo.hide();
         } else {
-            this.layer.add(this.buttons.next_round);
-            this.layer.add(this.images.shuttle_nogo);
+            this.buttons.init_shuttle.hide();
+            this.images.shuttle.hide();
+            this.images.travel_arrows.hide();
+            this.buttons.next_round.show();
+            this.images.shuttle_nogo.show();
         }
 
         this.layer.draw();
@@ -98,25 +148,32 @@ class Display {
         this.images['background'] = new Konva.Image({
             image: images.background,
         });
+        this.images['colonization_phase_indicator'] = new Konva.Image({
+            x: 0.202 * this.width,
+            y: 0.129 * this.height,
+            image: images.colonization_phase_indicator,
+        });
+        this.images['shuttle_phase_indicator'] = new Konva.Image({
+            x: 0.202 * this.width,
+            y: 0.129 * this.height,
+            image: images.shuttle_phase_indicator,
+        });
         this.images['shuttle'] = new Konva.Image({
             image: images.shuttle,
-            x: this.stage.width() / 2 - images.shuttle.width / 2,
-            y: 581,
+            //XXX x: 0.457 * this.width, downloaded image dimensions does not match assets in Zeplin 
+            //XXX y: 0.83 * this.height, downloaded image dimensions does not match assets in Zeplin 
+            x: 0.457 * this.width,
+            y: 0.82 * this.height,
         });
-        this.images['shuttle_go_left'] = new Konva.Image({
-            image: images.shuttle_go_left,
-            x: 71,
-            y: 587,
-        });
-        this.images['shuttle_go_right'] = new Konva.Image({
-            image: images.shuttle_go_right,
-            x: 260,
-            y: 587,
+        this.images['travel_arrows'] = new Konva.Image({
+            image: images.travel_arrows,
+            x: 0.183 * this.width,
+            y: 0.834 * this.height,
         });
         this.images['shuttle_nogo'] = new Konva.Image({
             image: images.shuttle_nogo,
-            x: this.stage.width() / 2 - images.shuttle_nogo.width / 2,
-            y: 581,
+            x: 0.454 * this.width,
+            y: 0.825 * this.height,
         });
 
         // call this from here because we need the images for the buttons
@@ -144,161 +201,107 @@ class Display {
     setupLabels() {
         this.labels['status'] = new Konva.Text({
             x: 0,
-            y: 94,
-            width: this.stage.width(),
+            y: 0.084 * this.height,
+            width: this.width,
             align: 'center',
             fontFamily: 'Continuum Medium Regular',
-            fontSize: 16,
+            fontSize: 36,
             text: '',
             fill: '#E5E5DE'
         });
         this.labels['action'] = new Konva.Text({
             x: 0,
-            y: this.stage.height() / 2 + 20,
-            width: this.stage.width(),
+            y: 0.52 * this.height, 
+            width: this.width,
             align: 'center',
             fontFamily: 'Continuum Medium Regular',
-            fontSize: 62,
+            fontSize: 147,
             text: '1',
             fill: 'white'
         });
         this.labels['mission'] = new Konva.Text({
             x: 0,
-            y: 148,
-            width: this.stage.width(),
+            y: 0.175 * this.height,
+            width: this.width,
             align: 'center',
             fontFamily: 'Continuum Medium Regular',
-            fontSize: 58,
-            text: '/',
+            fontSize: 108,
+            text: '',
             fill: 'white'
         });
     }
 
     setupButtons(images) {
         // REVEAL START POSITION
-        var label1 = new Konva.Text({
-            x: 0,
-            y: 24,
-            width: images.button_reveal.width,
-            align: 'center',
-            fontFamily: 'Continuum Medium Regular',
-            fontSize: 14,
-            padding: 1.5,
-            text: 'REVEAL START POSITION',
-            fill: '#24C5A3'
+        this.buttons['reveal'] = new Konva.Image({
+            image: images.init_shuttle,
+            //XXX x: 0.224 * this.width, downloaded image dimensions does not match assets in Zeplin 
+            x: this.width/2 - images.init_shuttle.width/2,
+            y: 0.888 * this.height,
         });
-        var image1 = new Konva.Image({
-            image: images.button_reveal,
-        });
-        this.buttons['reveal'] = new Konva.Group({
-            x: this.stage.width() / 2 - images.button_reveal.width / 2,
-            y: 623,
-        });
-        this.buttons['reveal'].add(image1);
-        this.buttons['reveal'].add(label1);
         this.buttons['reveal'].on('click tap', () => {
             this.revealStartPosition();
         });
 
         // FIRST ROUND
-        var label2 = new Konva.Text({
-            x: 0,
-            y: 12,
-            width: images.button_next.width - 9,
-            align: 'center',
-            fontFamily: 'Continuum Medium Regular',
-            fontSize: 14,
-            padding: 1.5,
-            text: 'FIRST ROUND',
-            fill: '#0E4B47'
+        this.buttons['first_round'] = new Konva.Image({
+            image: images.first_round,
+            //XXX x: 0.265 * this.width, downloaded image dimensions does not match assets in Zeplin 
+            x: this.width/2 - images.first_round.width/2,
+            y: 0.902 * this.height,
         });
-        var image2 = new Konva.Image({
-            image: images.button_next
-        });
-        this.buttons['first_round'] = new Konva.Group({
-            x: this.stage.width() / 2 - images.button_next.width / 2 + 5,
-            y: 635,
-        });
-        this.buttons['first_round'].add(image2);
-        this.buttons['first_round'].add(label2);
         this.buttons['first_round'].on('click tap', () => {
-            this.lacerda.nextState();
             this.layer.destroyChildren();
+            this.setupLayer();
+
+            this.lacerda.nextState();
+            this.renderScreen();
+        });
+
+        // BACK
+        this.buttons['backbutton'] = new Konva.Image({
+            image: images.backbutton,
+            x: 0.057 * this.width,
+            y: 0.016 * this.height,
+        });
+        this.buttons['backbutton'].on('click tap', () => {
+            this.lacerda.prevState();
             this.renderScreen();
         });
 
         // NEXT ROUND
-        var label3 = new Konva.Text({
-            x: 0,
-            y: 12,
-            width: images.button_next.width,
-            align: 'center',
-            fontFamily: 'Continuum Medium Regular',
-            fontSize: 14,
-            padding: 1.5,
-            text: 'NEXT ROUND',
-            fill: '#0E4B47'
+        this.buttons['next_round'] = new Konva.Image({
+            image: images.next_round,
+            //XXX x: 0.252 * this.width, downloaded image dimensions does not match assets in Zeplin 
+            x: this.width/2 - images.next_round.width/2,
+            y: 0.901 * this.height,
         });
-        var image3 = new Konva.Image({
-            image: images.button_next
-        });
-        this.buttons['next_round'] = new Konva.Group({
-            x: this.stage.width() / 2 - images.button_next.width / 2 + 5,
-            y: 635,
-        });
-        this.buttons['next_round'].add(image3);
-        this.buttons['next_round'].add(label3);
         this.buttons['next_round'].on('click tap', () => {
-            alert('1337');
+            this.lacerda.nextState();
+            this.renderScreen();
         });
 
         // INITIATE SHUTTLE PHASE
-        var label4 = new Konva.Text({
-            x: 0,
-            y: 24,
-            width: images.button_reveal.width,
-            align: 'center',
-            fontFamily: 'Continuum Medium Regular',
-            fontSize: 14,
-            padding: 1.5,
-            text: 'INITIATE SHUTTLE PHASE',
-            fill: '#24C5A3'
+        this.buttons['init_shuttle'] = new Konva.Image({
+            image: images.shuttle_phase_button,
+            //XXX x: 0.224 * this.width, downloaded image dimensions does not match assets in Zeplin
+            x: this.width/2 - images.shuttle_phase_button.width/2,
+            y: 0.888 * this.height,
         });
-        var image4 = new Konva.Image({
-            image: images.button_reveal
-        });
-        this.buttons['init_shuttle'] = new Konva.Group({
-            x: this.stage.width() / 2 - images.button_reveal.width / 2,
-            y: 623,
-        });
-        this.buttons['init_shuttle'].add(image4);
-        this.buttons['init_shuttle'].add(label4);
         this.buttons['init_shuttle'].on('click tap', () => {
-            alert('1338');
+            this.lacerda.currentPhase = 'SHUTTLE';
+            this.renderScreen();
         });
     }
 
     setupShapes() {
         this.shapes['active_mission'] = new Konva.Circle({
-            x: this.stage.width() / 2,
-            y: 176,
-            radius: 33,
+            x: 0.5 * this.width, //0.409 + 0.091 
+            y: 0.158 * this.height + 0.091 * this.width,
+            radius: 0.091 * this.width,
             fill: '#cf4541',
         });
-        this.shapes['inactive_mission'] = new Konva.Circle({
-            x: this.stage.width() / 2,
-            y: 176,
-            radius: 33,
-            fill: '#aaa9a2',
-        });
-
-        this.shapes['rect1'] = new Konva.Rect({ x: 17, y: 435, width: 25, height: 15, cornerRadius: 2, fill: '#aaa9a2' });
-        this.shapes['rect2'] = new Konva.Rect({ x: 17, y: 455, width: 25, height: 15, cornerRadius: 2, fill: '#aaa9a2' });
-        this.shapes['rect3'] = new Konva.Rect({ x: 17, y: 475, width: 25, height: 15, cornerRadius: 2, fill: '#aaa9a2' });
-        this.shapes['rect4'] = new Konva.Rect({ x: 17, y: 495, width: 25, height: 15, cornerRadius: 2, fill: '#aaa9a2' });
-        this.shapes['rect5'] = new Konva.Rect({ x: 17, y: 515, width: 25, height: 15, cornerRadius: 2, fill: '#aaa9a2' });
-        this.shapes['rect6'] = new Konva.Rect({ x: 17, y: 535, width: 25, height: 15, cornerRadius: 2, fill: '#aaa9a2' });
-
+        
         this.shapes['dark_square'] = new Konva.Rect({ y: 584, width: 27, height: 27, fill: '#1F867E' });
         this.shapes['light_square'] = new Konva.Rect({ y: 584, width: 27, height: 27, fill: '#619089' });
     }
